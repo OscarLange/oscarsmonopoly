93527bf5d7856891298976d956550dcee6f46216
Oscar Lange <langeoscar96@googlemail.com>
2020-12-13 10:51:39 +0100

    Added DataBase access

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Oscars Monopoly' });
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
+    const collection = client.db("monopoly").collection("games");
+    collection.findOne({}, function(err, result) {
+      if (err) throw err;
+      res.render('index', { title: result.name });
+      client.close();
+    });
+  });
 });
 
 module.exports = router;
